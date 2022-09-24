83e333019e97eda3f3599fcd3e95730ec6810c78
Sathvik-Malgikar <102981556+Sathvik-Malgikar@users.noreply.github.com>
2022-09-25 03:46:33 +0530

    Update retrieveDonation.js

--- src/retrieveDonation.js
+++ src/retrieveDonation.js
@@ -1,56 +1,60 @@
-async function loadweb3(){
- 
-    if (typeof web3 !== 'undefined') {
-      web3Provider = web3.currentProvider
-      web3 = new Web3(web3.currentProvider)
-    } else {
-      window.alert("Please connect to Metamask.")
-    }
-    // Modern dapp browsers...
-    if (window.ethereum) {
-      window.web3 = new Web3(ethereum)
-      try {
-        // Request account access if needed
-        await ethereum.enable()
-        // Acccounts now exposed
-        web3.eth.sendTransaction({/* ... */})
-      } catch (error) {
-        // User denied account access...
-      }
-    }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-      web3Provider = web3.currentProvider
-      window.web3 = new Web3(web3.currentProvider)
-      // Acccounts always exposed
-      web3.eth.sendTransaction({/* ... */})
-    }
-    // Non-dapp browsers...
-    else {
-      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+async function loadweb3() {
+  if (typeof web3 !== "undefined") {
+    web3Provider = web3.currentProvider;
+    web3 = new Web3(web3.currentProvider);
+  } else {
+    window.alert("Please connect to Metamask.");
+  }
+  // Modern dapp browsers...
+  if (window.ethereum) {
+    window.web3 = new Web3(ethereum);
+    try {
+      // Request account access if needed
+      await ethereum.enable();
+      // Acccounts now exposed
+      web3.eth.sendTransaction({
+        /* ... */
+      });
+    } catch (error) {
+      // User denied account access...
     }
   }
- 
-async function loadAccount(){
-account = web3.eth.accounts[0]
-console.log(account)
+  // Legacy dapp browsers...
+  else if (window.web3) {
+    web3Provider = web3.currentProvider;
+    window.web3 = new Web3(web3.currentProvider);
+    // Acccounts always exposed
+    web3.eth.sendTransaction({
+      /* ... */
+    });
+  }
+  // Non-dapp browsers...
+  else {
+    console.log(
+      "Non-Ethereum browser detected. You should consider trying MetaMask!"
+    );
+  }
+}
+
+async function loadAccount() {
+  account = web3.eth.accounts[0];
+  console.log(account);
 }
 
 load = async () => {
-await loadweb3()
-await loadAccount()
-await loadContract()
-
-}
+  await loadweb3();
+  await loadAccount();
+  await loadContract();
+};
 if (window.load()) {
-    load()
+  load();
 }
 
-async function loadContract(){
-    contractjson = await $.getJSON("retrieve.json")
-    Contractjson = TruffleContract(contractjson)
-    Contractjson.setProvider(web3Provider)
-    deployed = await Contractjson.deployed()
-    donations = deployed.donations
-    console.log(donations[0])
-    }
+async function loadContract() {
+  contractjson = await $.getJSON("retrieve.json");
+  Contractjson = TruffleContract(contractjson);
+  Contractjson.setProvider(web3Provider);
+  deployed = await Contractjson.deployed();
+  donations = deployed.donations;
+  console.log(donations['0']);
+}
