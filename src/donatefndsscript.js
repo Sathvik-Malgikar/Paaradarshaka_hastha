0b1cc670f159b91d92be32d4f9dccb208370ff02
Hemabhushan-r <hemabhushanr3@gmail.com>
2022-09-24 18:38:47 +0530

    donateFunds Changes

--- src/donatefndsscript.js
+++ src/donatefndsscript.js
@@ -3,11 +3,22 @@ mbox = document.getElementById("mbox")
 
 function changeMoney(event) {
 if(event.deltaY>=0){
-    console.log("red")
-    mbox.value =  (parseInt( mbox.value )-1) + " ETH";
+    //console.log("red")
+    mbox.value =  (parseInt( mbox.value )-1);
+    if(parseInt(mbox.value)<0){
+      mbox.value="0";
+    }
+    else if(parseInt(mbox.value)>100){
+      mbox.value="100"
+    }
 }else{
-    console.log("incr")
-  mbox.value =  (parseInt( mbox.value )+1 ) +" ETH";
-
+    //console.log("incr")
+  mbox.value =  (parseInt( mbox.value )+1 );
+  if(parseInt(mbox.value)<0){
+    mbox.value="0";
+  }
+  else if(parseInt(mbox.value)>100){
+    mbox.value="100"
+  }
 }
 }
