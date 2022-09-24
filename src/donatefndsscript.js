bd63bca09708f05c8317ffcd0c0ca4fc2b10475a
Sathvik-Malgikar <102981556+Sathvik-Malgikar@users.noreply.github.com>
2022-09-24 18:25:51 +0530

    removed plus minus

--- src/donatefndsscript.js
+++ src/donatefndsscript.js
@@ -3,10 +3,8 @@ mbox = document.getElementById("mbox")
 
 function changeMoney(event) {
 if(event.deltaY>=0){
-    console.log("red")
     mbox.value =  (parseInt( mbox.value )-1) + " ETH";
 }else{
-    console.log("incr")
   mbox.value =  (parseInt( mbox.value )+1 ) +" ETH";
 
 }
