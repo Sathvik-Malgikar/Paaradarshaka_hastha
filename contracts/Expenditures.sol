a420a685e97656ce67fc6b0c81f05cdb6e16a15e
Sathvik-Malgikar <102981556+Sathvik-Malgikar@users.noreply.github.com>
2022-09-24 21:21:30 +0530

    added expenditures

---
+++ contracts/Expenditures.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.5.0;
+
+contract Expenditures{
+
+uint public totalitems;
+
+  struct expenditures{
+        uint amt;
+        string month;
+        string donation_id;
+        bool spent;
+    }
+    
+
+    mapping(uint=>Expenditures) public donations;
+
+
+}
